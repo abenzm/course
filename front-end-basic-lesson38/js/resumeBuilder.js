01e16b0feef66b05046e4f04e11f1910b3023ea9
Myron <myron.z.zhang@gmail.com>
2017-04-14 15:31:40 +0000

    Add lesson38

---
+++ front-end-basic-lesson38/js/resumeBuilder.js
@@ -0,0 +1,40 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+var bio = {
+  "name" : "Myron",
+  "role" : "Engineera",
+  "contact" : "18566680567",
+  "pic_url" : "http://1.1.1.1/i.pic",
+  "welcom" : "Hello, world",
+  "skill" : ["computer", "swimming", "driving"],
+};
+var work = {
+  "Position": "Software Manager",
+  "employer": "YYD Robot",
+  "yearWorked": "19 years",
+  "city": "Shenzhen",
+};
+var education = {
+  "name": "Nanjing University of Science and Technology",
+  "attend": "1994",
+  "city": "Nanjing",
+  "major": "Electronic",
+  "minors": "Economics",
+  "graduationYear": 1998,
+  "onlineCourse": "",
+};
+var project = {
+  "projects": [
+    {
+      "title": "YOS2",
+      "dates": "Mar 2017",
+      "description": "Yongyida robot OS 2"
+    },
+    {
+      "title": "YOS1",
+      "dates": "Apr 2016",
+      "description": "Yongyida robot OS 1"
+    }
+  ]
+};
