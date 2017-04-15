742be04d30bd842d6883ddd67b93e25b267447c6
Myron <myron.z.zhang@gmail.com>
2017-04-15 05:59:56 +0000

    commit all lesson 23 files.

--- front-end-basic-lesson23/gulpfile.js
+++ front-end-basic-lesson23/gulpfile.js
@@ -1,12 +1,60 @@
 var gulp = require('gulp');
 var gm = require('gulp-gm');
+var gSequence= require('gulp-sequence');
 
-gulp.task('default', function() {
+gulp.task('resize1600', function() {
   gulp.src('img_src/*.jpg')
     .pipe(gm(function(gmfile) {
       return gmfile.resize(1600); 
     }, {
       imageMagick: true
     }))
-    .pipe(gulp.dest('img/'))
+    .pipe(gulp.dest('img/1600/'))
+});
+
+gulp.task('resize800', function() {
+  gulp.src('img_src/*.jpg')
+    .pipe(gm(function(gmfile) {
+      return gmfile.resize(800); 
+    }, {
+      imageMagick: true
+    }))
+    .pipe(gulp.dest('img/800/'))
+});
+
+gulp.task('resize500', function() {
+  gulp.src('img_src/*.jpg')
+    .pipe(gm(function(gmfile) {
+      return gmfile.resize(500); 
+    }, {
+      imageMagick: true
+    }))
+    .pipe(gulp.dest('img/500/'))
+});
+
+gulp.task('resize1000', function() {
+  gulp.src('img_src/*.jpg')
+    .pipe(gm(function(gmfile) {
+      return gmfile.resize(1000); 
+    }, {
+      imageMagick: true
+    }))
+    .pipe(gulp.dest('img/1000/'))
+});
+
+gulp.task('default', function (callback) {
+  gSequence(
+    'resize500',
+    'resize800',
+    'resize1000',
+    'resize1600',
+    function (error) {
+      if (error) {
+        console.log(error.message);
+      } else {
+        console.log('RELEASE FINISHED SUCCESSFULLY.');
+      }
+      callback(error);
+    }
+  );
 });
