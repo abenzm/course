9ba5004507adf2564289daf1c7bed57a07d45a98
Myron <myron.z.zhang@gmail.com>
2017-06-03 11:08:10 +0000

    Add lesson3 file

---
+++ python-lesson3/mindstorms.py
@@ -0,0 +1,24 @@
+import turtle
+
+def draw_someshape():
+    window = turtle.Screen()
+    windown.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("green")
+    brad.speed(0.5)
+
+    for i in range(1, 5):
+        brad.forward(100)
+        brad.right(90)
+
+    nie = turtle.Turtle()
+    nie.sharp("arrow")
+    nie.color("blue")
+    nie.circle(50)
+
+
+    window.exitonclick()
+
+draw_someshape()
